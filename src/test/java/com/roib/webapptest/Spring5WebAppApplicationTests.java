7ba18e9f72a8cbd218c63bcab203e6c6f5e77f1e
roibar81 <baraziroi@gmail.com>
2023-05-15 22:49:08 +0300

    first commit

---
+++ src/test/java/com/roib/webapptest/Spring5WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roib.webapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
