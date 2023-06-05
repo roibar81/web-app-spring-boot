05ce852e2bab7ff0df61601cd5dfd443e3cca50c
roibar81 <baraziroi@gmail.com>
2023-06-05 17:45:32 +0300

    add list.html file which represents list of author + AuthorController.

---
+++ src/main/java/com/roib/springboot/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package com.roib.springboot.controller;
+
+import com.roib.springboot.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+
+}
--- src/main/java/com/roib/springboot/domain/Author.java
+++ src/main/java/com/roib/springboot/domain/Author.java
@@ -13,6 +13,14 @@ import java.util.Set;
 @Entity
 public class Author {
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
